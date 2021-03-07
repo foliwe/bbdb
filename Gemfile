be4037e28d9a99541f7de39c12f6b6382c20df92
foliwe <foliwe@gmail.com>
2021-03-07 12:05:48 +0000

    devise and email

---
+++ Gemfile
@@ -0,0 +1,48 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+ruby '3.0.0'
+
+gem 'rails', '~> 6.1.3'
+gem 'pg', '~> 1.1'
+gem 'puma', '~> 5.0'
+gem 'sass-rails', '>= 6'
+gem 'webpacker', '~> 5.0'
+gem 'turbolinks', '~> 5'
+gem 'jbuilder', '~> 2.7'
+gem 'devise'
+gem 'wicked'
+gem 'cancancan'
+gem 'countries', '~> 1.2', '>= 1.2.5'
+gem 'rack-cors'
+gem 'sendinblue'
+gem 'image_processing'
+gem 'searchkick'
+gem 'aws-sdk-s3', require:false
+gem 'sidekiq', '~> 5.2.5'
+gem 'redis', '~> 4.0'
+gem 'bootsnap', '>= 1.4.4', require: false
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+
+
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+end
+
+group :development do
+  gem 'web-console', '>= 4.1.0'
+  gem 'rack-mini-profiler', '~> 2.0'
+  gem 'listen', '~> 3.3'
+  gem 'spring'
+  gem 'faker'
+  gem 'mailcatcher'
+  gem 'ruby-progressbar'
+end
+
+group :test do
+  gem 'capybara', '>= 3.26'
+  gem 'selenium-webdriver'
+  gem 'webdrivers'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ db/seeds/02_static_user.rb
@@ -0,0 +1,35 @@
+require 'faker'
+
+# progressbar = ProgressBar.create(
+#     title: 'Creating Users',
+#     total: BUSINESSES_TO_CREATE
+# )
+
+# 20.times do 
+#     Category.create!(name: Faker::Company.industry)
+# end
+
+User.create!(first_name: "Foliwe", 
+                  last_name: "Example",
+                   email: "foliwe@example.com",
+                   password: 'password',
+                   password_confirmation:"password")
+User.create!(first_name: "Akuma", 
+                  last_name: "Example",
+                   email: "akuma@example.com",
+                   password: 'password',
+                   password_confirmation:"password")
+
+User.create!(first_name: "Patrick", 
+                  last_name: "Example",
+                   email: "patrick@example.com",
+                   password: 'password',
+                   password_confirmation:"password")
+
+#     9.times do 
+#         Business.create!(business_name: Faker::Company.name,
+#             user_id: rand(1...4),number_of_employee:rand(1...101),category_id:rand(1...31),accepts_partnership: rand(2),business_email: Faker::Internet.email,business_address: Faker::Address.street_address,description: Faker::Lorem.paragraph(sentence_count: 2),mobile: Faker::PhoneNumber.cell_phone_in_e164,phone: Faker::PhoneNumber.cell_phone,zip_code: Faker::Address.zip_code )
+# end
+
+puts "3 Static Users created"
+puts "+++++++++++++++++++++++++++++++++++++++++"

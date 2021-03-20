b93b7647588755649248af5d656ce6b06f6cd5b4
foliwe <foliwe@gmail.com>
2021-03-20 04:52:17 +0000

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :users ,only: [:show]
-=======
   resources :users do 
     resources :profile ,only: [:index]
   end
   get 'users/show'
->>>>>>> userProfile
   resources :businesses do 
     resources :photos do
       match '/remove', to: 'businesses#remove', via: 'delete'

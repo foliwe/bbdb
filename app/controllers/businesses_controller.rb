fb01395b1f205ec123570279882694181e26d2da
foliwe <foliwe@gmail.com>
2021-03-25 14:39:53 +0000

    friendly_id

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -16,6 +16,7 @@ class BusinessesController < ApplicationController
 
   # GET /businesses/1 or /businesses/1.json
   def show
+    
   end
 
   # GET /businesses/new
@@ -82,7 +83,7 @@ class BusinessesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_business
-    @business = Business.find(params[:id])
+    @business = Business.friendly.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.

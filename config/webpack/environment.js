c967691a7c88f81dbe0be3101961fcb148f47fc6
foliwe <foliwe@gmail.com>
2021-03-25 13:02:35 +0000

    update

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,8 +2,11 @@ const {environment} = require('@rails/webpacker');
 
 const webpack = require('webpack');
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery' 
+  // $: 'jquery',
+  // jQuery: 'jquery' ,
+  $: 'jquery/src/jquery',
+  jQuery: 'jquery/src/jquery',
+  selectize: '?'
 }));
 
 module.exports = environment;

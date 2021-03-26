d2a02511b679f1a6d11e27b6da87427820cc93c8
foliwe <foliwe@gmail.com>
2021-03-26 16:18:35 +0000

    recaptcha

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 gem "friendly_id", "~> 5.4"
 gem "rolify"
 gem "pundit"
+gem 'recaptcha'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,7 +1,17 @@
 class User::RegistrationsController < Devise::RegistrationsController
+        prepend_before_action :check_captcha, only: :create
 
-    def after_sign_up_path_for(resource)
-        after_signup_path(:terms)
+        def after_sign_up_path_for(resource)
+            after_signup_path(:terms)
+        end
+    private
+
+    def check_captcha
+        unless verify_recaptcha
+            self.resource = resource_class.new sign_up_params
+            resource.validate 
+            set_minimum_password_length
+            respond_with_navigational(resource){render :new}
+        end
     end
-
 end

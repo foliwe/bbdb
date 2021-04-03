7c11a9c7da636291f717ecfa59c5799dae479444
foliwe <foliwe@gmail.com>
2021-04-03 14:31:15 +0100

    wizard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
 
-  def business_logo(business_id)
-    business = Business.find(business_id)
-    if user.logo.attached?
-        image_tag user.logo
-    else
-        image_tag 'default.png'
-    end
-end
 end

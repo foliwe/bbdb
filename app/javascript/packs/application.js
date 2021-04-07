3e4a1655ab41443a6edd7809d9c9fd5641ec1ebf
foliwe <foliwe@gmail.com>
2021-04-07 06:49:21 +0100

    advert

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,5 +25,3 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
-
-
